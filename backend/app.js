42fa83d247ec6fdc862ab6c3959d71bb93d65820
korcin <mkozera024@gmail.com>
2022-12-15 13:15:45 +0100

    Authorization process

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,12 @@ app.use(express.json())
 
 //Import wszystkich ścieżek
 const products = require("./routes/product")
+const auth = require("./routes/auth")
 
 app.use("/api/v1", products)
+app.use("/api/v1", auth)
 
 //Oprogramowanie pośredniczące do obsługi błędów
-app.use(errorMiddleware) 
+app.use(errorMiddleware)
 
 module.exports = app
---
+++ backend/controllers/authController.js
@@ -0,0 +1,23 @@
+const User = require("../models/user")
+const ErrorHandler = require("../utils/errorHandler")
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors")
+
+//Rejestracja użytkownika => /api/v1/register
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+	const { name, email, password } = req.body
+
+	const user = await User.create({
+		name,
+		email,
+		password,
+		avatar: {
+			public_id: "avatars/avatar1_e19tqg",
+			url: "https://res.cloudinary.com/dn4sa6fc2/image/upload/v1671105140/avatars/avatar1_e19tqg.jpg",
+		},
+	})
+    res.status(201).json({
+        success: true,
+        user,
+    })
+})
+
