6311d902cb7e7d0eb7471e38280a07412f6cf735
Marcin Kozera <79635513+korcin@users.noreply.github.com>
2022-12-23 21:59:51 +0100

    Update Profile

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -160,6 +160,24 @@ exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
 	sendToken(user, 200, res)
 })
 
+// Zaktualizuj profil użytkownika => /api/v1/me/update
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+	const newUserData = {
+		name: req.body.name,
+		email: req.body.email,
+	}
+
+	// Aktualizacja avatara profilu
+	const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+		new: true,
+		runValidators: true,
+		useFindAndModify: false,
+	})
+	res.status(200).json({
+		success: true,
+	})
+})
+
 // Wylogowywanie użytkownika => /api/v1/logout
 exports.logout = catchAsyncErrors(async (req, res, next) => {
 	res.cookie("token", null, {
