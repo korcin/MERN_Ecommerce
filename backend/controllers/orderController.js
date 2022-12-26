6a66f086bfdaa5567d45c83b5f8f600c803ba57f
Marcin Kozera <79635513+korcin@users.noreply.github.com>
2022-12-26 16:08:47 +0100

    Order routes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -32,3 +32,46 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
 		order,
 	})
 })
+
+// Znajdź pojedyncze zamówienie => /api/v1/order/:id
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+	const order = await Order.findById(req.params.id).populate(
+		"user",
+		"name email"
+	)
+
+	if (!order) {
+		return next(new ErrorHandler("Nie znaleziono takiego zamówienia.", 400))
+	}
+
+	res.status(200).json({
+		success: true,
+		order,
+	})
+})
+
+// Znajdź zamówienia zalogowanego użytkownika => /api/v1/orders/me
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+	const orders = await Order.find({ user: req.user.id })
+
+	res.status(200).json({
+		success: true,
+		orders,
+	})
+})
+
+// Znajdź wszystkie zamówienia => /api/v1/admin/orders
+exports.allOrders = catchAsyncErrors(async (req, res, next) => {
+	const orders = await Order.find()
+
+	let totalAmount = 0
+	orders.forEach(order => {
+		totalAmount += order.totalPrice
+	})
+
+	res.status(200).json({
+		success: true,
+		totalAmount,
+		orders,
+	})
+})
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const router = express.Router()
 
-const { newOrder } = require("../controllers/orderController")
+const {
+	newOrder,
+	getSingleOrder,
+	myOrders,
+    allOrders,
+} = require("../controllers/orderController")
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth")
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder)
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder)
+router.route("/orders/me").get(isAuthenticatedUser, myOrders)
+
+// ADMIN
+router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles("admin"), allOrders)
 
 module.exports = router
