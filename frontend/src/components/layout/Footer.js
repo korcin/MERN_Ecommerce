a8324fed3215415eef19d4159a661961fd86bae2
Marcin Kozera <79635513+korcin@users.noreply.github.com>
2022-12-28 00:22:56 +0100

    Add footer

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react"
+
+const Footer = () => {
+	return (
+		<Fragment>
+			<footer className='py-1'>
+				<p className='text-center mt-1'>
+					Shopping Cart - 2022-2023, Wszystkie prawa zastrzeżone
+				</p>
+			</footer>
+		</Fragment>
+	)
+}
+
+export default Footer
