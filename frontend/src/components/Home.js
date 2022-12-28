eb4dea235c627623f873a81b14bd73ea0ccf0c4d
Marcin Kozera <79635513+korcin@users.noreply.github.com>
2022-12-28 13:06:48 +0100

    Custom titles

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,11 @@
-import React from "react"
+import React, { Fragment } from "react"
+import MetaData from "./layout/MetaData"
 
 const Home = () => {
 	return (
-		<div classNameName='container-sm container-fluid'>
+		<Fragment>
+			<MetaData title={'Kup najlepsze produkty online'} />
+			
 			<h1 id='products_heading'>Najnowsze produkty</h1>
 			<section id='products' className='container mt-5'>
 				<div className='row'>
@@ -32,7 +35,7 @@ const Home = () => {
 					</div>
 				</div>
 			</section>
-		</div>
+		</Fragment>
 	)
 }
 
