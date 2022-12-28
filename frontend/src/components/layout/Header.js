eb0d722f01e267d3b122f8a0bfba3dd3adc458c5
Marcin Kozera <79635513+korcin@users.noreply.github.com>
2022-12-28 13:06:59 +0100

    Correcting style

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
 					</div>
 				</div>
 
-				<div className='col-12 col-md-6 mt-2 mt-md-0'>
+				<div className='col-12 col-md-6 mt-2 mt-md-0 '>
 					<div className='input-group'>
 						<input
 							type='text'
@@ -27,7 +27,7 @@ const Header = () => {
 				</div>
 
 				<div className='col-12 col-md-3 mt-4 mt-md-0 text-center'>
-					<button className='btn' id='login_btn'>
+					<button className='btn me-3' id='login_btn'>
 						Zaloguj
 					</button>
 
