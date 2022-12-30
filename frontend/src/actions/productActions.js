0036e68b6f881d608405e76d1dddd8a5f13a42e3
Marcin Kozera <79635513+korcin@users.noreply.github.com>
2022-12-30 18:25:32 +0100

    Product details component

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
 	ALL_PRODUCTS_REQUEST,
 	ALL_PRODUCTS_SUCCESS,
 	ALL_PRODUCTS_FAIL,
+	PRODUCT_DETAILS_REQUEST,
+	PRODUCT_DETAILS_SUCCESS,
+	PRODUCT_DETAILS_FAIL,
 	CLEAR_ERRORS,
 } from "../constants/productConstants"
 
@@ -24,9 +27,27 @@ export const getProducts = () => async dispatch => {
 	}
 }
 
+export const getProductDetails = id => async dispatch => {
+	try {
+		dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+		const { data } = await axios.get(`/api/v1/product/${id}`)
+
+		dispatch({
+			type: PRODUCT_DETAILS_SUCCESS,
+			payload: data.product,
+		})
+	} catch (error) {
+		dispatch({
+			type: PRODUCT_DETAILS_FAIL,
+			payload: error.response.data.message,
+		})
+	}
+}
+
 // Wyczyść błędy
-export const clearErrors = () => async (dispatch) => {
-    dispatch({
-        type: CLEAR_ERRORS
-    })
+export const clearErrors = () => async dispatch => {
+	dispatch({
+		type: CLEAR_ERRORS,
+	})
 }
---
+++ frontend/src/components/product/ProductDetails.js
@@ -0,0 +1,180 @@
+import React, { Fragment, useEffect } from "react"
+import Loader from "../layout/Loader"
+import MetaData from "../layout/MetaData"
+import { Carousel } from "react-bootstrap"
+import { useAlert } from "react-alert"
+import { useDispatch, useSelector } from "react-redux"
+import { getProductDetails, clearErrors } from "../../actions/productActions"
+import { useParams } from "react-router-dom"
+
+const ProductDetails = ({ match }) => {
+	const dispatch = useDispatch()
+	const alert = useAlert()
+	const { id } = useParams()
+	const { loading, error, product } = useSelector(state => state.productDetails)
+
+	useEffect(() => {
+		dispatch(getProductDetails(id))
+
+		if (error) {
+			alert.error(error)
+			dispatch(clearErrors())
+		}
+	}, [dispatch, alert, error, id])
+
+	return (
+		<Fragment>
+			{loading ? (
+				<Loader />
+			) : (
+				<Fragment>
+					<MetaData title={product.name} />
+					<div className='row f-flex justify-content-around'>
+						<div className='col-12 col-lg-5 img-fluid' id='product_image'>
+							<Carousel pause='hover'>
+								{product.images &&
+									product.images.map(image => (
+										<Carousel.Item key={image.public_id}>
+											<img
+												className='d-block w-100 h-100'
+												src={image.url}
+												alt={product.title}
+											/>
+										</Carousel.Item>
+									))}
+							</Carousel>
+						</div>
+						<div className='col-12 col-lg-5 mt-5'>
+							<h3>{product.name}</h3>
+							<p id='product_id'>Kod produktu #{product._id}</p>
+
+							<hr />
+
+							<div className='rating-outer'>
+								<div
+									className='rating-inner'
+									style={{
+										width: `${(product.ratings / 5) * 100}%`,
+									}}></div>
+							</div>
+							<span id='no_of_reviews'>({product.numOfReviews} Opinii)</span>
+
+							<hr />
+
+							<p id='product_price'>{product.price} zł</p>
+							<div className='stockCounter d-inline'>
+								<span className='btn btn-danger minus'>-</span>
+
+								<input
+									type='number'
+									className='form-control count d-inline'
+									value='1'
+									readOnly
+								/>
+
+								<span className='btn btn-primary plus'>+</span>
+							</div>
+							<button
+								type='button'
+								id='cart_btn'
+								className='btn btn-primary d-inline ms-3'>
+								Dodaj do koszyka
+							</button>
+
+							<hr />
+
+							<p>
+								Status:{" "}
+								<span
+									id='stock_status'
+									className={product.stock > 0 ? "greenColor" : "redColor"}>
+									{product.stock > 0 ? "Dostępny" : "Niedostępny"}
+								</span>
+							</p>
+
+							<hr />
+
+							<h4 className='mt-2'>Opis:</h4>
+							<p>{product.description}</p>
+							<hr />
+							<p id='product_seller mb-3'>
+								Producent: <strong>{product.seller}</strong>
+							</p>
+
+							<button
+								id='review_btn'
+								type='button'
+								className='btn btn-primary mt-4'
+								data-bs-toggle='modal'
+								data-bs-target='#ratingModal'>
+								Dodaj opinię
+							</button>
+
+							<div className='row mt-2 mb-5'>
+								<div className='rating w-50'>
+									<div
+										className='modal fade'
+										id='ratingModal'
+										tabIndex='-1'
+										role='dialog'
+										aria-labelledby='ratingModalLabel'
+										aria-hidden='true'>
+										<div className='modal-dialog' role='document'>
+											<div className='modal-content'>
+												<div className='modal-header'>
+													<h5 className='modal-title' id='ratingModalLabel'>
+														Treść opinii
+													</h5>
+													<button
+														type='button'
+														className='btn-close'
+														data-bs-dismiss='modal'
+														aria-label='Close'></button>
+												</div>
+												<div className='modal-body'>
+													<ul className='stars'>
+														<li className='star'>
+															<i className='fa fa-star'></i>
+														</li>
+														<li className='star'>
+															<i className='fa fa-star'></i>
+														</li>
+														<li className='star'>
+															<i className='fa fa-star'></i>
+														</li>
+														<li className='star'>
+															<i className='fa fa-star'></i>
+														</li>
+														<li className='star'>
+															<i className='fa fa-star'></i>
+														</li>
+													</ul>
+
+													<textarea
+														name='review'
+														id='review'
+														className='form-control mt-3'></textarea>
+
+													<div className='modal-footer'>
+														<button
+															className='my-3 float-right submit-btn px-4 text-white'
+															data-bs-dismiss='modal'
+															aria-label='Close'>
+															Dodaj opinię
+														</button>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</Fragment>
+			)}
+		</Fragment>
+	)
+}
+
+export default ProductDetails
