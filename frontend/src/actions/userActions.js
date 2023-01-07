62d1f8e3f72c42118f145d46c42ed95e10cf57c3
korcin <mkozera024@gmail.com>
2023-01-07 19:45:25 +0100

    Update user profile reducer & action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -11,7 +11,11 @@ import {
 	LOAD_USER_FAIL,
 	LOGOUT_SUCCESS,
 	LOGOUT_FAIL,
+	UPDATE_PROFILE_REQUEST,
+	UPDATE_PROFILE_SUCCESS,
+	UPDATE_PROFILE_RESET,
 	CLEAR_ERRORS,
+	UPDATE_PROFILE_FAIL,
 } from "../constants/userConstants"
 
 // Logowanie
@@ -82,6 +86,31 @@ export const loadUser = () => async dispatch => {
 	} catch (error) {
 		dispatch({
 			type: LOAD_USER_FAIL,
+			payload: error.response.data.message,
+		})
+	}
+}
+
+// Aktualizacja profilu
+export const updateProfile = userData => async dispatch => {
+	try {
+		dispatch({ type: UPDATE_PROFILE_REQUEST })
+
+		const config = {
+			headers: {
+				"Content-Type": "multipart/form-data",
+			},
+		}
+
+		const { data } = await axios.put("/api/v1/me/update", userData, config)
+
+		dispatch({
+			type: UPDATE_PROFILE_SUCCESS,
+			payload: data.success,
+		})
+	} catch (error) {
+		dispatch({
+			type: UPDATE_PROFILE_FAIL,
 			payload: error.response.data.message,
 		})
 	}
