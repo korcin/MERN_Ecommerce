c32633091c27951b6dcc8680830ebb7fbfb76d4a
korcin <mkozera024@gmail.com>
2023-01-07 16:50:21 +0100

    Register user component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import Header from "./components/layout/Header"
 import Home from "./components/Home"
 import Footer from "./components/layout/Footer"
 import ProductDetails from "./components/product/ProductDetails"
-import Login from './components/user/Login'
+import Login from "./components/user/Login"
+import Register from "./components/user/Register"
 
 import "./App.css"
 
@@ -18,6 +19,7 @@ function App() {
 						<Route path='/search/:keyword' element={<Home />} />
 						<Route path='/product/:id' element={<ProductDetails />} />
 						<Route path='/login' element={<Login />} />
+						<Route path='/register' element={<Register />} />
 					</Routes>
 				</div>
 				<Footer />
