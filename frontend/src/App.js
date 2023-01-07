434b303f5b2bd10c0f517ecd09a2d8a445a6e535
korcin <mkozera024@gmail.com>
2023-01-07 17:19:36 +0100

    Load logged in user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
+import { useEffect } from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
 import Header from "./components/layout/Header"
 import Home from "./components/Home"
+
 import Footer from "./components/layout/Footer"
 import ProductDetails from "./components/product/ProductDetails"
+
 import Login from "./components/user/Login"
 import Register from "./components/user/Register"
+
+import { loadUser } from "./actions/userActions"
+import store from "./store"
 
 import "./App.css"
 
 function App() {
+	useEffect(() => {
+		store.dispatch(loadUser())
+	}, [])
+
 	return (
 		<Router>
 			<div className='App'>
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -6,6 +6,9 @@ import {
 	REGISTER_USER_REQUEST,
 	REGISTER_USER_SUCCESS,
 	REGISTER_USER_FAIL,
+	LOAD_USER_REQUEST,
+	LOAD_USER_SUCCESS,
+	LOAD_USER_FAIL,
 	CLEAR_ERRORS,
 } from "../constants/userConstants"
 
@@ -39,7 +42,7 @@ export const login = (email, password) => async dispatch => {
 }
 
 // Rejestracja
-export const register = (userData) => async dispatch => {
+export const register = userData => async dispatch => {
 	try {
 		dispatch({ type: REGISTER_USER_REQUEST })
 
@@ -49,8 +52,7 @@ export const register = (userData) => async dispatch => {
 			},
 		}
 
-		const { data } = await axios.post(
-			"/api/v1/register", userData, config)
+		const { data } = await axios.post("/api/v1/register", userData, config)
 
 		dispatch({
 			type: REGISTER_USER_SUCCESS,
@@ -59,6 +61,25 @@ export const register = (userData) => async dispatch => {
 	} catch (error) {
 		dispatch({
 			type: REGISTER_USER_FAIL,
+			payload: error.response.data.message,
+		})
+	}
+}
+
+// Zalogowany użytkownik
+export const loadUser = () => async dispatch => {
+	try {
+		dispatch({ type: LOAD_USER_REQUEST })
+
+		const { data } = await axios.get("/api/v1/me")
+
+		dispatch({
+			type: LOAD_USER_SUCCESS,
+			payload: data.user,
+		})
+	} catch (error) {
+		dispatch({
+			type: LOAD_USER_FAIL,
 			payload: error.response.data.message,
 		})
 	}
