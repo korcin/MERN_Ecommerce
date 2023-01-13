0c2e051fc7afb044c58030e03e1b07b4c062b62c
korcin <mkozera024@gmail.com>
2023-01-13 02:47:16 +0100

    Limits for pictures

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,17 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(fileUpload())
+app.use(express.json({ limit: "50mb" }))
+app.use(bodyParser.json({ limit: "50mb" }))
+app.use(
+	bodyParser.urlencoded({
+		limit: "50mb",
+		extended: true,
+		parameterLimit: 50000,
+	})
+)
+app.use(cookieParser())
+app.use(fileUpload())
 
 //Import wszystkich ścieżek
 const products = require("./routes/product")
